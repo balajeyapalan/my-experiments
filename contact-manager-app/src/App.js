7a73d25f807cde22434080b700eb5aff69c29019
Bala Palanichamy <balamurugan.jeyapalan@gmail.com>
2021-06-06 12:18:43 +0530

    New components created

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -4,16 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         This is contact-manager-app
-        </a>
-      </header>
+      This is contact-manager-app
     </div>
   );
 }
